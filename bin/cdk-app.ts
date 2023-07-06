224d642fca0372a888b996e85c58ad4cb1b4cca6
Shivin Gaba <shivingaba@gmail.com>
2023-07-06 12:09:34 +1200

    Playaround with CDK stacks

--- bin/cdk-app.ts
+++ bin/cdk-app.ts
@@ -18,4 +18,17 @@ new CdkAppStack(app, 'CdkAppStack', {
   // env: { account: '123456789012', region: 'us-east-1' },
 
   /* For more information, see https://docs.aws.amazon.com/cdk/latest/guide/environments.html */
+
+    env:{
+        account: process.env.CDK_DEFAULT_ACCOUNT,
+        region: process.env.CDK_DEFAULT_REGION
+    }
+});
+
+new CdkAppStack(app, 'cdk-stack-prod', {
+    stackName: 'cdk-stack-prod',
+    env: {
+        account: process.env.CDK_DEFAULT_ACCOUNT,
+        region: process.env.CDK_DEFAULT_REGION,
+    },
 });
--- lib/cdk-app-stack.ts
+++ lib/cdk-app-stack.ts
@@ -1,5 +1,8 @@
 import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
+import * as s3 from 'aws-cdk-lib/aws-s3';
+import * as dynamodb from 'aws-cdk-lib/aws-dynamodb'
+import {aws_dynamodb} from "aws-cdk-lib";
 // import * as sqs from 'aws-cdk-lib/aws-sqs';
 
 export class CdkAppStack extends cdk.Stack {
@@ -7,6 +10,25 @@ export class CdkAppStack extends cdk.Stack {
     super(scope, id, props);
 
     // The code that defines your stack goes here
+
+    //Bucket Construct
+    const bucket = new s3.Bucket(this, 'avatar-bucket', {
+      removalPolicy: cdk.RemovalPolicy.DESTROY,
+    })
+
+    //Table construct
+    const table = new dynamodb.Table(this, 'todos-table', {
+      partitionKey: {name: 'id', type: dynamodb.AttributeType.NUMBER},
+      billingMode: dynamodb.BillingMode.PAY_PER_REQUEST,
+      removalPolicy: cdk.RemovalPolicy.DESTROY,
+    })
+
+    //Output Construct
+    new cdk.CfnOutput(this, 'bucketName',{
+      value: bucket.bucketName,
+    });
+    new cdk.CfnOutput(this, 'tableName', {value: table.tableName})
+
 
     // example resource
     // const queue = new sqs.Queue(this, 'CdkAppQueue', {
